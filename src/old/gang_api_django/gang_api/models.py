0a9374b4c68a84e7be377b1074b8dd3ab4adacd4
Jarek Lipski <loomchild@loomchild.net>
2013-07-03 23:50:57 +0200

    Removed old directory - not needed anymore in VCS

--- src/old/gang_api_django/gang_api/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class Issue(models.Model):
-	summary = models.CharField(max_length=200)
-	description = models.CharField(max_length=2000)
-	amount = models.IntegerField()
-
