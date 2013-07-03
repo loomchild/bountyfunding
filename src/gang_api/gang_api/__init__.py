f9de3504df9cad9bbe508ec1fd4c184734dd2d98
Jarek Lipski <loomchild@loomchild.net>
2013-07-03 23:57:02 +0200

    Moved database to relative db directory to avoid recreating it every time I want to start development

--- src/gang_api/gang_api/__init__.py
+++ src/gang_api/gang_api/__init__.py
@@ -1,4 +1,8 @@
+from os import path
 from flask import Flask
+
 app = Flask(__name__)
+
+GANG_HOME=path.abspath(path.join(path.dirname(path.abspath(__file__)), '..', '..', '..'))
 
 import gang_api.views
