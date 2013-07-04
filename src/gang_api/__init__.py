700fe4f2193903d5d08433338fe226e14c9ae27e
Jarek Lipski <loomchild@loomchild.net>
2013-07-04 14:15:16 +0200

    Moved plugins to a subdirectory

--- src/gang_api/__init__.py
+++ src/gang_api/__init__.py
@@ -3,6 +3,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-GANG_HOME=path.abspath(path.join(path.dirname(path.abspath(__file__)), '..', '..', '..'))
+GANG_HOME=path.abspath(path.join(path.dirname(path.abspath(__file__)), '..', '..'))
 
 import gang_api.views
