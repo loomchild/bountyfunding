bad3311fedd806b59a985e9be9a9a05b026eca33
Jarek Lipski <loomchild@loomchild.net>
2013-10-07 14:03:30 +0200

    Add more functional tests.

--- src/functional_test/simple_test.py
+++ src/functional_test/simple_test.py
@@ -1,10 +1,52 @@
 from utils import api
 from nose.tools import *
+from collections import namedtuple
 
 
-def test():
+def test_retrieving_nonexisting_issue_returns_404():
 	r = api.get("/issue/1")
 	eq_(r.status_code, 404)
-	r = api.post('/issue/1/sponsorships', user='loomchild', amount=10)
+
+def test_sponsor_nonexisting_issue_creates_it():
+	user = 'loomchild'
+	amount = 10
+
+	r = api.get("/issue/1")
+	eq_(r.status_code, 404)
+	r = api.post('/issue/1/sponsorships', user=user, amount=amount)
 	r = api.get("/issue/1")
 	eq_(r.status_code, 200)
+	issue = dict_to_object(r.json())
+	eq_(issue.status, 'NEW')
+
+	r = api.get("/issue/1/sponsorships")
+	eq_(r.status_code, 200)
+	sponsorships = r.json()
+	eq_(len(sponsorships), 1)
+	sponsorship = dict_to_object(sponsorships[user])
+	eq_(sponsorship.amount, amount) 
+	eq_(sponsorship.status, 'PLEDGED') 
+
+def test_sponsor_existing_issue_updates_it():
+	user = 'pralinka'
+	amount = 30
+	
+	r = api.post('/issue/1/sponsorships', user=user, amount=amount)
+	r = api.get("/issue/1/sponsorships")
+	eq_(r.status_code, 200)
+
+	sponsorships = r.json()
+	eq_(len(sponsorships), 2)
+	sponsorship = dict_to_object(sponsorships[user])
+	eq_(sponsorship.amount, amount) 
+	eq_(sponsorship.status, 'PLEDGED') 
+
+def test_sponsor_issue_by_same_user_updates_sponsorship():
+	pass
+
+
+def dict_to_object(d):
+	o = namedtuple('DictObject', d.keys())(*d.values())
+	return o
+
+
