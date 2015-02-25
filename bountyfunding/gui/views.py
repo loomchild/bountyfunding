da16c782344c9b13b6905ae043a72852f6fd1942
Jarek Lipski <loomchild@loomchild.net>
2015-02-25 22:51:50 +0100

    use relative pathis in blueprints

--- bountyfunding/gui/views.py
+++ bountyfunding/gui/views.py
@@ -8,7 +8,7 @@ from flask_bootstrap import Bootstrap
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
-login_manager.login_view = 'gui.login'
+login_manager.login_view = '.login'
 
 
 @gui.record_once
